56d5b026d160cb7dfcc7404b68c65b2011ad96c8
cucmberslice <ywblackberry@gmail.com>
2019-01-16 14:39:09 -0500

    graphQL dependecies

---
+++ src/db.js
@@ -0,0 +1,12 @@
+/**
+ * In this tutorial we’re not using a 
+ * real-world database. So this db.js 
+ * file is going to simulate a database, 
+ * just for learning purposes.
+ */
+
+
+export let users = [
+    { id: 1, name: 'John Doe', email: 'john@gmail.com', age: 22 },
+    { id: 2, name: 'Jane Doe', email: 'jane@gmail.com', age: 23}
+]
---
+++ src/resolvers.js
@@ -0,0 +1,60 @@
+/**
+ * But, you may be wondering: how does GraphQL 
+ * know where get the data? That’s 
+ * why we should have a resolvers.js file. 
+ * That file tells GraphQL how and where it's 
+ * going to fetch the data.
+ * 
+ * 
+ * This resolvers.js file is 
+ * going to be the way we provide 
+ * the instructions for turning a GraphQL 
+ * operation into data.
+ */
+
+
+import { users } from "./db";
+
+const resolvers = {
+    Query: {
+       user: (parent, { id }, context, info) => {
+           return users.find(user => user.id === id);
+       },
+
+       users: (parent, args, context, info) => {
+           return users;
+       }
+    },
+
+    Mutation: {
+        createUser: (parent, { id, name, email, age }, context, info) => {
+            const newUser = { id, name, email, age };
+
+            users.push(newUser);
+
+            return newUser;
+        },
+
+        updateUser: (parent, { id, name, email, age }, context, info) => {
+            let newUser = users.find(user => user.id === id);
+
+            newUser.name = name;
+            newUser.email = email;
+            newUser.age = age;
+
+            return newUser;
+        },
+
+        deleteUser: (parent, { id }, context, info) => {
+            const userIndex = users.findIndex(user => user.id === id);
+
+            if(userIndex === -1) throw new Error("User not found.");
+
+            const deleteUsers = users.splice(userIndex, 1);
+
+            return deleteUsers[0]
+        }
+    }
+}
+
+export default resolvers;
